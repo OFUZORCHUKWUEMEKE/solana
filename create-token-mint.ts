238e8dd8131669776792527aec7f41199831372e
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-07-29 17:28:05 +0100

    transfer Mint

---
+++ create-token-mint.ts
@@ -0,0 +1,96 @@
+import { createMint } from "@solana/spl-token";
+import * as dotenv from "dotenv";
+import { getKeypairFromEnvironment, getExplorerLink } from "@solana-developers/helpers";
+import { Connection, Keypair, PublicKey, Transaction, clusterApiUrl, sendAndConfirmTransaction } from "@solana/web3.js";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+dotenv.config();
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const keypair = Keypair.generate();
+
+console.log(
+    `🔑 Loaded our keypair securely, using an env file! Our public key is: ${keypair.publicKey.toBase58()}`
+);
+
+const tokenMint = await createMint(connection, keypair, keypair.publicKey, null, 2);
+const link = getExplorerLink("address", tokenMint.toString(), "devnet");
+
+console.log(`✅ Finished! Created token mint: ${link}`);
+
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
+// Subtitute in your token mint account
+const tokenMintAccount = new PublicKey("YOUR_TOKEN_MINT_ADDRESS_HERE");
+
+const metadataData = {
+    name: "Solana Training Token",
+    symbol: "TRAINING",
+    // Arweave / IPFS / Pinata etc link using metaplex standard for off-chain data
+    uri: "https://arweave.net/1234",
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+};
+
+const metadataPDAAndBump = PublicKey.findProgramAddressSync(
+    [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        tokenMintAccount.toBuffer(),
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+);
+
+const metadataPDA = metadataPDAAndBump[0];
+
+const transaction = new Transaction();
+
+const createMetadataAccountInstruction =
+    createCreateMetadataAccountV3Instruction(
+        {
+            metadata: metadataPDA,
+            mint: tokenMintAccount,
+            mintAuthority: keypair.publicKey,
+            payer: keypair.publicKey,
+            updateAuthority: keypair.publicKey,
+        },
+        {
+            createMetadataAccountArgsV3: {
+                collectionDetails: null,
+                data: metadataData,
+                isMutable: true,
+            },
+        }
+    );
+
+transaction.add(createMetadataAccountInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [keypair]
+);
+
+const transactionLink = getExplorerLink(
+    "transaction",
+    transactionSignature,
+    "devnet"
+);
+
+console.log(`✅ Transaction confirmed, explorer link is: ${transactionLink}!`);
+
+const tokenMintLink = getExplorerLink(
+    "address",
+    tokenMintAccount.toString(),
+    "devnet"
+);
+
+console.log(`✅ Look at the token mint again: ${tokenMintLink}!`);
+
+
+
+
--- transfer.ts
+++ transfer.ts
@@ -5,21 +5,24 @@ const keypair = Keypair.generate()
 
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 const address = new PublicKey(keypair.publicKey.toBase58());
+const balance = await connection.getBalance(address);
 const reciepient = new PublicKey(`CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN`);
 const transaction = new Transaction();
 
-await airdropIfRequired(
-    connection,
-    keypair.publicKey,
-    1 * LAMPORTS_PER_SOL,
-    0.5 * LAMPORTS_PER_SOL,
-);
+console.log(balance);
+
+// await airdropIfRequired(
+//     connection,
+//     keypair.publicKey,
+//     1 * LAMPORTS_PER_SOL,
+//     0.5 * LAMPORTS_PER_SOL,
+// );
 
 const sendSolInstuction = SystemProgram.transfer({
     fromPubkey: address,
     toPubkey: reciepient,
     lamports: LAMPORTS_PER_SOL * 1
-})
+});
 transaction.add(sendSolInstuction);
 const signature = sendAndConfirmTransaction(
     connection,
@@ -29,4 +32,4 @@ const signature = sendAndConfirmTransaction(
     console.log("Transaction sent with signature:", signature)
 }).catch((error) => {
     console.log("Error sending transaction:", error);
-})
+});
